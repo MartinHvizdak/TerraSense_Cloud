fb8d7d0576f75bde25c4207eb6924113018ee92a
marti <martinhvizdak2@gmail.com>
2023-04-25 11:31:59 +0200

    Initial commit

---
+++ src/main/java/sep4/terrasense_cloud/TerraSenseCloudApplication.java
@@ -0,0 +1,13 @@
+package sep4.terrasense_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraSenseCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerraSenseCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sep4/terrasense_cloud/TerraSenseCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package sep4.terrasense_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraSenseCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
