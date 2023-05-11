e1e3a3a970dfa874e1a4af1839399998ec0ab7d0
marti <martinhvizdak2@gmail.com>
2023-05-11 15:28:47 +0200

    Added Rest API connection, database connection still needs to be tested since it's not filled with readings

--- src/main/java/sep4/terrasense_cloud/Controller/ReadingController.java
+++ src/main/java/sep4/terrasense_cloud/Controller/ReadingController.java
@@ -1,13 +1,11 @@
 package sep4.terrasense_cloud.Controller;
 
 import org.springframework.web.bind.annotation.*;
-import sep4.terrasense_cloud.database.repository.ReadingsRepository;
 import sep4.terrasense_cloud.model.Reading;
 import sep4.terrasense_cloud.service.impl.ReadingsServiceImpl;
-import sep4.terrasense_cloud.service.services.ReadingsService;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/reading")
@@ -18,11 +16,23 @@ public class ReadingController {
     public ReadingController(ReadingsServiceImpl readingsService) {
         this.readingsService=readingsService;
     }
-
+/*
     @GetMapping("/")
     public ArrayList<Reading> getLastReadings(@RequestParam("quantity") int quantity){
         return readingsService.getReadings(quantity);
     }
+*/
+    @GetMapping("/")
+    @ResponseBody
+    public ArrayList<Reading> getReadingsByTimestamps(@RequestParam("start") String startDate,
+                                                      @RequestParam("end") String endDate)
+    {
+        System.out.println(startDate);
+        System.out.println(endDate);
+        // The expected format is YYYY-mm-dd
+        return readingsService.getReadingsByTimestamps(LocalDate.parse(startDate), LocalDate.parse(endDate));
+    }
+
 
     @GetMapping("/{id}")
     public Reading getReadingById(@PathVariable("id") long id){
