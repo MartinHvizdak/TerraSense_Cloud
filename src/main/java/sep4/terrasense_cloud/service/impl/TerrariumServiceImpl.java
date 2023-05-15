1590d1252db49e2112e01884a25429c40d91932f
marti <martinhvizdak2@gmail.com>
2023-05-15 13:16:49 +0200

    All endpoints implemented, working without problems

--- src/main/java/sep4/terrasense_cloud/service/impl/TerrariumServiceImpl.java
+++ src/main/java/sep4/terrasense_cloud/service/impl/TerrariumServiceImpl.java
@@ -1,9 +1,13 @@
 package sep4.terrasense_cloud.service.impl;
 
+import org.springframework.stereotype.Service;
 import sep4.terrasense_cloud.database.repository.TerrariumRepository;
 import sep4.terrasense_cloud.model.Terrarium;
 import sep4.terrasense_cloud.service.services.TerrariumService;
 
+import java.util.NoSuchElementException;
+
+@Service
 public class TerrariumServiceImpl implements TerrariumService {
 
     TerrariumRepository terrariumRepository;
@@ -15,5 +19,15 @@ public class TerrariumServiceImpl implements TerrariumService {
     @Override
     public Terrarium addTerrarium(Terrarium terrarium) {
         return terrariumRepository.save(terrarium);
+    }
+
+    @Override
+    public Terrarium getTerraruimById(Long id) {
+        try{
+            return terrariumRepository.findById(id).get();
+        }catch (NoSuchElementException e){
+            System.out.println(e.getStackTrace());
+            return new Terrarium();
+        }
     }
 }
