4cb6e458d638f3ef9b8a250aa49e9faf8f395c76
marti <martinhvizdak2@gmail.com>
2023-05-19 13:06:10 +0200

    Implementing registration with Argon2 hashing

--- src/main/java/sep4/terrasense_cloud/security/PasswordEncoder.java
+++ src/main/java/sep4/terrasense_cloud/security/PasswordEncoder.java
@@ -5,11 +5,7 @@ import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 
 public class PasswordEncoder {
-
-    private int salt = 10;
-    private int hashLength = 65536;
-
-    public Argon2 PasswordEncoder(){
-        return Argon2Factory.create(salt, hashLength);
+    public Argon2 getEncoder(){
+        return Argon2Factory.create();
     }
 }
--- src/test/java/sep4/terrasense_cloud/TerraSenseCloudApplicationTests.java
+++ src/test/java/sep4/terrasense_cloud/TerraSenseCloudApplicationTests.java
@@ -1,5 +1,7 @@
 package sep4.terrasense_cloud;
 
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +11,18 @@ class TerraSenseCloudApplicationTests {
   /*  @Test
     void contextLoads() {
     }*/
+/*
+    @Test
+    void testHashing(){
+        String hash = "$argon2i$v=19$m=65536,t=10,p=1$VX/tgvp4pd8pkLklGTnthw$VAUgGEDiF5Y4vwcnYSzrk+5Yw/kSFw/YDNeo2107On4";
 
+        Argon2 encoder = Argon2Factory.create();
+        int iterations = 10;
+        int memory = 65536;
+        int parallelism = 1;
+
+        System.out.println(encoder.verify(hash, "password"));
+    }
+
+ */
 }
