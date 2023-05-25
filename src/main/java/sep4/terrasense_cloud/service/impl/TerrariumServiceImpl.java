f6abc37371a5db7da1d6d5a4a226717933b74c23
ZoT1YaK <robert.zustars@gmail.com>
2023-05-25 17:17:38 +0200

    Implementing terrarium management

--- src/main/java/sep4/terrasense_cloud/service/impl/TerrariumServiceImpl.java
+++ src/main/java/sep4/terrasense_cloud/service/impl/TerrariumServiceImpl.java
@@ -1,7 +1,12 @@
 package sep4.terrasense_cloud.service.impl;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sep4.terrasense_cloud.database.repository.CustomerRepository;
 import sep4.terrasense_cloud.database.repository.TerrariumRepository;
+import sep4.terrasense_cloud.model.Customer;
+import sep4.terrasense_cloud.model.LimitsDTO;
 import sep4.terrasense_cloud.model.Terrarium;
 import sep4.terrasense_cloud.service.services.TerrariumService;
 
@@ -12,8 +17,11 @@ public class TerrariumServiceImpl implements TerrariumService {
 
     TerrariumRepository terrariumRepository;
 
-    public TerrariumServiceImpl(TerrariumRepository terrariumRepository) {
+    CustomerRepository customerRepository;
+
+    public TerrariumServiceImpl(TerrariumRepository terrariumRepository, CustomerRepository customerRepository) {
         this.terrariumRepository = terrariumRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
@@ -28,6 +36,64 @@ public class TerrariumServiceImpl implements TerrariumService {
         }catch (NoSuchElementException e){
             System.out.println(e.getStackTrace());
             return new Terrarium();
+        }
+    }
+
+    @Override
+    public Terrarium createTerrarium(Terrarium terrarium, String email){
+        try {
+            Customer customer = customerRepository.findById(email).get();
+            Terrarium findTerrarium  = terrariumRepository.findById(1L).get();
+            findTerrarium.setMinCO2(terrarium.getMinCO2());
+            findTerrarium.setMaxCO2(terrarium.getMaxCO2());
+            findTerrarium.setMinHumidity(terrarium.getMinHumidity());
+            findTerrarium.setMaxHumidity(terrarium.getMaxHumidity());
+            findTerrarium.setMinTemperature(terrarium.getMinTemperature());
+            findTerrarium.setMaxTemperature(terrarium.getMaxTemperature());
+            findTerrarium.setUser(customer);
+            terrariumRepository.save(findTerrarium);
+            return findTerrarium;
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+            return new Terrarium();
+        }
+    }
+
+    @Transactional
+    @Override
+    public void alterTerrarium(String email, Terrarium terrarium){
+        try {
+            Customer customer = customerRepository.findById(email).get();
+
+            Terrarium findTerrarium = terrariumRepository.getTerrariumByCustomerAndId(customer, 1L);
+
+            findTerrarium.setName(terrarium.getName());
+            findTerrarium.setMinCO2(terrarium.getMinCO2());
+            findTerrarium.setMaxCO2(terrarium.getMaxCO2());
+            findTerrarium.setMinHumidity(terrarium.getMinHumidity());
+            findTerrarium.setMaxHumidity(terrarium.getMaxHumidity());
+            findTerrarium.setMinTemperature(terrarium.getMinTemperature());
+            findTerrarium.setMaxTemperature(terrarium.getMaxTemperature());
+
+            terrariumRepository.alterTerrarium(findTerrarium.getId(), findTerrarium.getName(),
+                    findTerrarium.getMinTemperature(), findTerrarium.getMaxTemperature() , findTerrarium.getMinHumidity(),
+            findTerrarium.getMaxHumidity(), findTerrarium.getMinCO2(), findTerrarium.getMaxCO2());
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+    }
+
+    @Transactional
+    @Override
+    public void deleteTerrarium(String email, Long terrariumId){
+        try {
+            Customer customer = customerRepository.findById(email).get();
+
+            Terrarium findTerrarium = terrariumRepository.getTerrariumByCustomerAndId(customer, terrariumId);
+
+            terrariumRepository.deleteTerrarium(findTerrarium.getId());
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
         }
     }
 }
--- src/main/java/sep4/terrasense_cloud/service/services/TerrariumService.java
+++ src/main/java/sep4/terrasense_cloud/service/services/TerrariumService.java
@@ -1,10 +1,18 @@
 package sep4.terrasense_cloud.service.services;
 
 import org.springframework.stereotype.Service;
+import sep4.terrasense_cloud.model.Customer;
+import sep4.terrasense_cloud.model.LimitsDTO;
 import sep4.terrasense_cloud.model.Terrarium;
 
 @Service
 public interface TerrariumService {
     Terrarium addTerrarium(Terrarium terrarium);
     Terrarium getTerraruimById(Long id);
+
+    Terrarium createTerrarium(Terrarium terrarium, String email);
+
+    void alterTerrarium(String email, Terrarium terrarium);
+
+    void deleteTerrarium(String email, Long terrariumId);
 }
